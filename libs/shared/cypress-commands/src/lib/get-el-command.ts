0da9a5bc5100506557b56fea9636b6e7c81a61ea
Hiep Luong <hiepluong2205@gmail.com>
2024-07-23 21:49:07 -0700

    add getElByExactText

--- libs/shared/cypress-commands/src/lib/get-el-command.ts
+++ libs/shared/cypress-commands/src/lib/get-el-command.ts
@@ -3,9 +3,16 @@ declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
     getEl(identifier: string): Cypress.Chainable<JQuery<HTMLElement>>;
+    getElByExactText(
+      selector: string,
+      text: string
+    ): Cypress.Chainable<JQuery<HTMLElement>>;
   }
 }
 
 Cypress.Commands.add('getEl', (identifier: string) => {
   return cy.get(`[data-testid="${identifier}"]`);
+});
+Cypress.Commands.add('getElByExactText', (selector: string, text: string) => {
+  return cy.contains(selector, new RegExp('^' + text + '$'));
 });
